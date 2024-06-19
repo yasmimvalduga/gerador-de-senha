349b89595c0b590d2afcadb357004617bbcfe454
yasmimvalduga <163342034+yasmimvalduga@users.noreply.github.com>
2024-06-19 16:11:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -22,11 +22,59 @@ function aumentaTamanho(){
     numeroSenha.textContent = tamanhoSenha;
 }
 
-console.log(botoes);
+numeroSenha . textContent = tamanhoSenha;
+gerador ( ) ;
+
+for (i=0; i < caixa de seleção . comprimento ;i++) {
+ caixa de seleção [ eu ]. onclick = geraSenha;
+}
+
+geraSenha () {
+    function geraSenha(){
+        let alfabeto = '';
+        if (checkbox[0].checked){
+            alfabeto = alfabeto + letrasMaiusculas;
+        }
+        if (checkbox[1].checked){
+            alfabeto = alfabeto + letrasMinusculas;
+        }
+        if (checkbox[2].checked){
+            alfabeto = alfabeto + numeros;
+        }
+        if (checkbox[3].checked){
+            alfabeto = alfabeto + simbolos;
+        }
+        console.log(alfabeto);
+        let senha = '';
+        for (let i = 0; i < tamanhoSenha;i++){
+            let numeroAleatorio = Math.random()*alfabeto.length;
+            numeroAleatorio = Math.floor(numeroAleatorio);
+            senha = senha + alfabeto[numeroAleatorio];
+        }
+        campoSenha.value = senha;
+    classificaSenha(alfabeto.legth);
+    }
+
+    function classificaSenha(tamanhoAlfabeto){
+        let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+        console.log(entropia);
+        forcaSenha.classList.remove('fraca','media','forte');
+        if (entropia > 57){
+            forcaSenha.classList.add('forte');
+        } else if (entropia > 35 && entropia < 57 ) {
+            forcaSenha.classList.add('media');
+        } else if (entropia <= 35){
+            forcaSenha.classList.add('fraca');
+        }
+        const valorEntropia = document.querySelector('.entropia');
+        valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+    }
+    
+       
+    
 
 
-
-
+}
 
 
 
